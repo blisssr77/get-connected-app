27d7e6d0813c5c2f8ec65f8291d9fac9337e9d84
blisssr77 <robinchae55@gmail.com>
2024-05-16 22:34:37 -0700

    wokring on freelancer page

--- frontend_getconnected/src/components/Pages/Freelancers.jsx
+++ frontend_getconnected/src/components/Pages/Freelancers.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import { AppContext } from '../../App'
 
-const Freelancer = () => {
+const Freelancer = (props) => {
+  const freelancers = useContext(AppContext);
+
+  
+
   return (
     <div className='pt-28'>Freelancer</div>
   )
